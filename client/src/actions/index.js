0d80f1cbeec6aa38e66c3420ac9818b3b933b8a2
Nguyenle23 <beenbin2308@gmail.com>
2023-04-14 17:28:47 +0700

    Nguyen | Build | Show data on UI

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -4,6 +4,11 @@ const BASE_URL = 'http://localhost:8080/';
 
 export const scanImage = async(imageSrc) => {
     const res = await axios.post(`${BASE_URL}scan/`, {imageSrc});
-    return res
+    return res;
+}
+
+export const getAllCustomer = async() => {
+    const res = await axios.get(`${BASE_URL}users/`);
+    return res;
 }
 
--- client/src/components/Table.jsx
+++ client/src/components/Table.jsx
@@ -1,62 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import classNames from "classnames/bind";
 import styles from "../styles/Table.module.css";
-
+import { getAllCustomer } from '../actions';
 
 const cx = classNames.bind(styles);
 
-const fakeData = [
-    {
-        index: 1,
-        plateNo: 123456,
-        plateType: 'Commercial',
-        date: '13.4.2023',
-        timeStamp: '22.59.16',
-        site: 'Ho Chi Minh City'
-    },
-    {
-        index: 2,
-        plateNo: 213456,
-        plateType: 'Commercial',
-        date: '13.4.2023',
-        timeStamp: '20.59.16',
-        site: 'Ha Noi'
-    },
-    {
-        index: 3,
-        plateNo: 456556,
-        plateType: 'Commercial',
-        date: '13.4.2023',
-        timeStamp: '14.59.16',
-        site: 'Ho Chi Minh City'
-    },
-    {
-        index: 4,
-        plateNo: "59A70B",
-        plateType: 'Commercial',
-        date: '13.4.2023',
-        timeStamp: '17.59.16',
-        site: 'Da Nang'
-    },
-    {
-        index: 5,
-        plateNo: 189856,
-        plateType: 'Commercial',
-        date: '13.4.2023',
-        timeStamp: '20.12.16',
-        site: 'Tra Vinh'
-    },
-    {
-        index: 6,
-        plateNo: 1555456,
-        plateType: 'Commercial',
-        date: '13.4.2023',
-        timeStamp: '22.19.42',
-        site: 'Thanh Hoa'
-    },
-]
-
 function Table() {
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        getAllCustomer().then(res => {
+            if (res.status === 200) {
+                const dataWithIndex = res.data.map((data, index) => {
+                    return {
+                        ...data,
+                        createdAt: new Date(data.createdAt).toLocaleDateString(),
+                        timeStamp: new Date(data.createdAt).toLocaleTimeString(),
+                        index: index + 1
+                    }
+                })
+                setData(dataWithIndex);
+            }
+        })
+    }, [])
+
     return (
         <div className={cx('table-container')}>
             <table>
@@ -64,13 +30,13 @@ function Table() {
                     <tr>
                         <th colSpan="7">
                             <div className={cx('interact-row')}>
-                                <span className={cx('records-number')}>3600 records</span>
-                                <span className={cx('rows-number')}>No of rows in table: X</span>
+                                <span className={cx('records-number')}>Records: {data.length}</span>
+                                <span className={cx('rows-number')}>No of rows in table: {data.length}</span>
                                 <select defaultValue={'Sortby'} className={cx('select-type')}>
                                     <option value="Sortby" disabled>Sort by</option>
                                     <option value="s-no">S.no</option>
+                                    <option value="customerName">Customer</option>
                                     <option value="plate-no">Plate No.</option>
-                                    <option value="plate-type">Plate type</option>
                                     <option value="date">Date</option>
                                     <option value="time-stamp">TimeStamp</option>
                                     <option value="site">Site</option>
@@ -83,27 +49,26 @@ function Table() {
                 <tbody>
                     <tr className={cx('header')}>
                         <td>S.no</td>
+                        <td>Customer</td>
                         <td>Plate No.</td>
-                        <td>Plate type</td>
+                        <td>Site</td>
                         <td>Date</td>
                         <td>Timestamp</td>
-                        <td>Site</td>
                         <td>Image</td>
                     </tr>
-                    {fakeData.map(data => (
+                    {data.map(data => (
                         <tr key={data.index}>
                             <td>{data.index}</td>
+                            <td>{data.phoneNumber}</td>
                             <td>{data.plateNo}</td>
-                            <td>{data.plateType}</td>
-                            <td>{data.date}</td>
-                            <td>{data.timeStamp}</td>
                             <td>{data.site}</td>
+                            <td>{data.createdAt}</td>
+                            <td>{data.timeStamp}</td>
                             <td>No img</td>
                         </tr>
                     ))}
                 </tbody>
             </table>
-
         </div>
     );
 }
