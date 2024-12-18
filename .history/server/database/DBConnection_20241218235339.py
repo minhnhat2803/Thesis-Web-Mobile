ed6cb01154f4375ceb947d223a18fe1c60129728
minhnhat2803 <150834119+nhmnhatai4ai@users.noreply.github.com>
2024-12-18 23:58:09 +0700

    update new

--- .history/server/database/DBConnection_20241218235339.py
+++ .history/server/database/DBConnection_20241218235339.py
@@ -1,7 +1,7 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-cred = credentials.Certificate('/Users/nguyenhuuminhnhat/Downloads/iot-smart-parking-72f94-firebase-adminsdk-6ubd4-bcf933035b.json') 
+cred = credentials.Certificate('') 
 #bỏ dấu chú thích ở dòng trên và thay đường dẫn file json của bạn vào đây
 #chỗ trong ngoặc kép thì lên firebase tạo file json chứa thông tin chứng chỉ rồi tạo đường dẫn của file json đã tải xuống dẫn vào đây
 firebase_admin.initialize_app(cred, {'storageBucket': 'iot-smart-parking-72f94.appspot.com'})
