ea48a4c8fc934b24f71cb3cb969c62cf52bd4774
minhnhat2803 <150834119+nhmnhatai4ai@users.noreply.github.com>
2025-01-08 19:14:45 +0700

    update

--- .history/mobile/lib/main_20241231213335.dart
+++ .history/mobile/lib/main_20241231213335.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
-import 'package:mobile/screens/login_screen.dart'; 
+import 'package:mobile/screens/login_screen.dart';  // Đảm bảo đường dẫn đúng
 
 void main(List<String> args) async {
   WidgetsFlutterBinding.ensureInitialized();
