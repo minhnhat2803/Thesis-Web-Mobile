0e0e74bc6baf538abffd86b49fef9fd20d8c7d42
minhnhat2803 <150834119+nhmnhatai4ai@users.noreply.github.com>
2025-02-04 14:04:10 +0700

    delete comment

--- .history/server/database/DBConnection_20250204140348.py
+++ .history/server/database/DBConnection_20250204140348.py
@@ -2,6 +2,7 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 
 cred = credentials.Certificate('/Users/nguyenhuuminhnhat/Downloads/iot-smart-parking-72f94-firebase-adminsdk-6ubd4-e01f36954a.json') 
+
 firebase_admin.initialize_app(cred, {'storageBucket': 'iot-smart-parking-72f94.appspot.com'})
 
 def DBConnection(collectionName):
